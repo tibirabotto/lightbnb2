5a861d33cc503c1d67213d63f12815ab5dbd2291
tibirabotto <tibira.botto@gmail.com>
2023-11-17 11:01:58 -0500

    feature: getUserWithId

--- db/database.js
+++ db/database.js
@@ -34,7 +34,14 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
